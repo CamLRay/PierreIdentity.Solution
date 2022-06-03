c886d462458cf5a059afe3f13fa05e3857727a28
Sploob <crayiphone@gmail.com>
2022-06-03 13:05:10 -0700

    add account controller and viewmodels

---
+++ PierreIdentity/ViewModels/LoginViewModel.cs
@@ -0,0 +1,9 @@
+namespace PierreIdentity.ViewModels
+{
+    public class LoginViewModel
+    {
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
